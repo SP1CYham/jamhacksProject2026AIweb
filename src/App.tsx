43acac8f776fc552e23e08bcfb8ef3270c859645
elliot <elliotweek@gmail.com>
2026-06-13 14:15:31 -0400

    parallax animation meow

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import { Dropdown } from "./components/Dropdown";
 import { SelectableList } from "./components/SelectableList";
 import { useQuestion, allAnswered } from "./components/useQuestion";
 
-//calculations
+
 import { Q1, Q3 } from "./CarbonCalc";
 
 export default function App() {
@@ -104,6 +104,7 @@ export default function App() {
 
   return (
     <>
+    <div className="parallax-bg" />
       <img src="/clock.png" alt="mascot" className="mascot-img" />
       <div
         style={{
