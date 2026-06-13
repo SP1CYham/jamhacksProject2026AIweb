95afdfc346cbf66c9d78e827d8b739288c5a4689
SP1CYham <kylehuang1107@gmail.com>
2026-06-13 18:12:27 -0400

    added paragraphs and 2nd page stuff

--- src/CarbonCalc.tsx
+++ src/CarbonCalc.tsx
@@ -13,3 +13,40 @@ export function Q3(usage: number[]) {
 }
 
 //after
+export function kWattHours(
+  prompts: number,
+  tier: number[],
+  usage: number[],
+  lengthConvo: number,
+  imgPercentage: number,
+) {
+  const tokens = prompts * 500;
+  const tierCo = [0.0002, 0.0006, 0.002, 0.02];
+  const usageCo = [2, 1, 1, 0.2, 0.1];
+
+  var tierF = 0;
+  for (let i = 0; i < tier.length; i++) {
+    tierF += tier[i] * tierCo[i];
+  }
+  tierF /= 100;
+
+  var usageF = 0;
+  for (let i = 0; i < usage.length; i++) {
+    usageF += usage[i] * usageCo[i];
+  }
+  usageF /= 100;
+
+  imgPercentage /= 100;
+
+  return (
+    (((tokens *
+      (1 - imgPercentage) *
+      tierF *
+      usageF *
+      (1 + (lengthConvo - 1) * 0.15) +
+      prompts * imgPercentage * 2.9) *
+      1.4) /
+      1000) *
+    365
+  );
+}
