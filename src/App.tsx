8879e30ae46f59df17abd94e2280d9da0a0edddd
elliot <elliotweek@gmail.com>
2026-06-13 16:44:41 -0400

    meow

--- src/App.tsx
+++ src/App.tsx
@@ -170,6 +170,7 @@ export default function App() {
             />
             <button
               type="button"
+              className="continue-btn"
               disabled={!canContinue}
               onClick={() => setSubmitted(true)}
               style={{
